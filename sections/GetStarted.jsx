c83089805c105d2f12163638f599d8e5f09cc4d1
Avijit <avijitsarkar2222@gmail.com>
2023-04-05 14:18:39 +0530

    creating the GetStarted Section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,7 +1,20 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
+import { exploreWorlds } from '../constants';
+
 const GetStarted = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+    ></motion.div>
     Get Started Section
   </section>
 );
